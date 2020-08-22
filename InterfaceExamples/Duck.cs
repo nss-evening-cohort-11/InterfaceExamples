16641d3ac1cdb6322b006d24658b9574d9d65f2f
Nathan Gonzalez <ncgonz@gmail.com>
2020-08-22 13:36:18 -0500

    Add project files.

---
+++ InterfaceExamples/Duck.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceExamples
+{
+    class Duck : IFlyable
+    {
+        public void Fly()
+        {
+            Console.WriteLine("Quack, I'm a flying duck");
+        }
+    }
+
+    class Airplane : ITakeOff
+    {
+        public void Fly()
+        {
+            Console.WriteLine("we're flying on a plane");
+        }
+
+        public void TakeOff(string destination)
+        {
+            Console.WriteLine($"Taking off to go to {destination}");
+        }
+    }
+
+    class Helicopter : ITakeOff
+    {
+        public void Fly()
+        {
+            Console.WriteLine("hovering in a helicopter");
+        }
+
+        public void TakeOff(string destination)
+        {
+            Console.WriteLine($"Taking off to go to {destination}");
+        }
+    }
+
+
+    interface IFlyable
+    {
+        void Fly();
+    }
+
+    interface ITakeOff : IFlyable
+    {
+        void TakeOff(string destination);
+    }
+
+
+}
---
+++ InterfaceExamples/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterfaceExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var duck = new Duck();
+            var plane = new Airplane();
+            var helicopter = new Helicopter();
+
+            var flyingThings = new List<IFlyable> { duck, plane, helicopter };
+
+            foreach (var thing in flyingThings)
+            {
+                if (thing is ITakeOff takeOff)
+                {
+                    takeOff.TakeOff("Bahamas");
+                }
+                
+                thing.Fly();
+            }
+
+
+        }
+    }
+}
